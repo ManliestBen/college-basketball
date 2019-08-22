d31a00bcd8b291444459ae5d4b916d342abd650a
ManliestBen <benjamintmanley@gmail.com>
2019-08-22 13:05:11 -0500

    add cloud database config info

--- config/database.js
+++ config/database.js
@@ -2,14 +2,11 @@ var mongoose = require('mongoose');
 
 
 
-// Uncomment the mongoose stuff once the database is
-// the cloud
-
-// mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 
 // database connection:
-// mongoose.connection.on('connected', function () {
-//     console.log(`Mongoose connected to: ${process.env.DATABASE_URL}`);
-// });
+mongoose.connection.on('connected', function () {
+    console.log(`Mongoose connected to: ${process.env.DATABASE_URL}`);
+});
 
 module.exports = mongoose;
