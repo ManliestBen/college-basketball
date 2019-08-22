baaba011a589f2870521e5b1890d51cbf696bacf
ManliestBen <benjamintmanley@gmail.com>
2019-08-22 12:27:18 -0500

    set up simple home page and nav bar within header/footer

--- config/database.js
+++ config/database.js
@@ -2,4 +2,14 @@ var mongoose = require('mongoose');
 
 
 
+// Uncomment the mongoose stuff once the database is
+// the cloud
+
+// mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+
+// database connection:
+// mongoose.connection.on('connected', function () {
+//     console.log(`Mongoose connected to: ${process.env.DATABASE_URL}`);
+// });
+
 module.exports = mongoose;
