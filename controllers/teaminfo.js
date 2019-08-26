57eaba414b8e09a80a393a4baeb5f9bb00a3919f
ManliestBen <benjamintmanley@gmail.com>
2019-08-26 16:46:45 -0500

    add axios, fix api data pull

--- controllers/teaminfo.js
+++ controllers/teaminfo.js
@@ -1,5 +1,6 @@
 const TeamInfo = require('../models/teaminfo');
 var request = require('request');
+const axios = require('axios');
 
 
 module.exports = {
@@ -13,7 +14,36 @@ function index(req, res) {
 }
 
 function show(req, res) {
-    TeamInfo.findById(req.params.id, function(err, users) {
-        res.render('teaminfo/show', {user:req.user, users});
+    let { id } =  req.params;
+    var apiLink = {
+        url: `
+        https://api.sportradar.us/ncaamb/trial/v4/en/teams/${id}/profile.json?api_key=${process.env.NCAAMB_TOKEN}`
+    }
+    axios.get(apiLink.url)
+    .then(result => {
+        console.log(result)
+        return result.data
+    })
+    .then(response => {
+        console.log('requesting data from API', apiLink.url);
+        console.log(response)
+        res.render('teaminfo/show', {user:req.user, response});
+
+    }).catch(error => console.error(error))
+}
+
+
+
+function teamList(req, res){{}
+  let { teamId } = req.params
+    var apiLink = {
+        url: 'https://api.sportradar.us/ncaawb-t3/games/2018/REG/schedule.json?api_key=' + process.env.NCAAWB_TOKEN
+    };
+    console.log('requesting data from API');
+    request(apiLink, function(err, response, body) {
+        var teamData = JSON.parse(body);
+        console.log(teamData);
+        res.render('teaminfo/', {user:req.user, teamData});
+       
     })
 }
--- models/teaminfo.js
+++ models/teaminfo.js
@@ -2,7 +2,6 @@ var mongoose = require('mongoose');
 
 var playerSchema = new mongoose.Schema({
     playerId: String,
-    teamId: String,
     playerName: String,
     playerHeight: Number,
     playerWeight: Number,
