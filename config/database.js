7d1a02850d179a7a6cb0474c56cd7ee90222eab3
ManliestBen <benjamintmanley@gmail.com>
2019-08-26 14:37:00 -0500

    fix env file

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
+console.log(process.env.DATABASE_URL)
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
-
 // database connection:
 mongoose.connection.on('connected', function () {
     console.log(`Mongoose connected to: ${process.env.DATABASE_URL}`);
--- models/teaminfo.js
+++ models/teaminfo.js
@@ -1,4 +1,3 @@
-var mongoose = require('mongoose');
 
 
 var playerSchema = new mongoose.Schema({
