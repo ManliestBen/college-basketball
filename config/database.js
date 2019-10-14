2274d0a8a736010300dae00b87ea9587191032a3
ManliestBen <benjamintmanley@gmail.com>
2019-10-14 18:08:30 -0500

    adjust env

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
-console.log(process.env.DATABASE_URL)
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+
+mongoose.connect(`mongodb+srv://sei:${process.env.DATABASE_URL}@sei-1hogc.gcp.mongodb.net/collegebasketball?retryWrites=true&w=majority`, { useNewUrlParser: true });
 // database connection:
 mongoose.connection.on('connected', function () {
     console.log(`Mongoose connected to: ${process.env.DATABASE_URL}`);
